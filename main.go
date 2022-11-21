e3fa86d0a5c693bc04c4179863b3cf0078279f8d
panigs7 <spandita.panigrahi@emc.com>
2022-11-21 12:49:16 -0500

    fix lint issues

--- main.go
+++ main.go
@@ -1,6 +1,6 @@
 /*
  Copyright © 2020 Dell Inc. or its subsidiaries. All Rights Reserved.
- 
+
  Licensed under the Apache License, Version 2.0 (the "License");
  you may not use this file except in compliance with the License.
  You may obtain a copy of the License at
@@ -11,15 +11,17 @@
  See the License for the specific language governing permissions and
  limitations under the License.
 */
+
 package main
 
 import (
 	"context"
 	"flag"
 	"fmt"
-	"github.com/dell/gocsi"
 	"os"
 	"strings"
+
+	"github.com/dell/gocsi"
 
 	"github.com/dell/csi-unity/k8sutils"
 	"github.com/dell/csi-unity/provider"
