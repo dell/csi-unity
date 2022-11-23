29fa5ebe8ddd1950de4f835d2d4a35eaa1a41258
panigs7 <spandita.panigrahi@emc.com>
2022-11-23 10:56:29 -0500

    place shebang at begining

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
+#!/bin/bash
 #  Copyright © 2020 Dell Inc. or its subsidiaries. All Rights Reserved.
- 
+# 
 #  Licensed under the Apache License, Version 2.0 (the "License");
 #  you may not use this file except in compliance with the License.
 #  You may obtain a copy of the License at
@@ -9,8 +10,7 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
-
-#!/bin/bash
+#
 # This script will build an image for the Unity CSI Driver
 # Before running this script, make sure that you have podman installed on your system
 # If you are going to push the image to an image repo, make sure that you are logged in
