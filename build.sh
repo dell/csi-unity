10b41c21f2d7d594abe4a65404a63aad2374a80a
Alik Saring <alik.saring@dell.com>
2023-11-07 10:29:38 -0500

    ubi9.3-6 image

--- build.sh
+++ build.sh
@@ -30,8 +30,8 @@ function git_version {
 
 
 function build_image {
-   # Tag corresponding to digest sha256:d14ac3ae12148f838511d08261e1569fb2a54da4c54a817aea7f16c1c9078f0b for ubi9 micro is 9.2-15
-   bash build_ubi_micro.sh registry.redhat.io/ubi9/ubi-micro@sha256:d14ac3ae12148f838511d08261e1569fb2a54da4c54a817aea7f16c1c9078f0b
+   # Tag corresponding to digest sha256:b7d8fd2840e92e8adc68414e13d859763ef33c3d4c4e27f910e939c00d642c29 for ubi9 micro is 9.3-6
+   bash build_ubi_micro.sh registry.access.redhat.com/ubi9/ubi-micro@sha256:b7d8fd2840e92e8adc68414e13d859763ef33c3d4c4e27f910e939c00d642c29
    echo $BUILDCMD build -t ${IMAGE_NAME}:${IMAGE_TAG} .
    (cd .. && $BUILDCMD build -t ${IMAGE_NAME}:${IMAGE_TAG} --build-arg GOPROXY=$GOPROXY -f csi-unity/Dockerfile.podman . --format=docker)
    echo $BUILDCMD tag ${IMAGE_NAME}:${IMAGE_TAG} ${IMAGE_REPO}/${IMAGE_REPO_NAMESPACE}/${IMAGE_NAME}:${IMAGE_TAG}
