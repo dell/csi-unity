768b3cecee823b45ebc2f8dd6fa834c696c68c7b
adarsh-dell <adarsh.kumaryadav@dell.com>
2024-05-20 06:00:41 -0400

    Version bump up

--- dell-csi-helm-installer/csi-install.sh
+++ dell-csi-helm-installer/csi-install.sh
@@ -16,10 +16,10 @@ PROG="${0}"
 NODE_VERIFY=1
 VERIFY=1
 MODE="install"
-DEFAULT_DRIVER_VERSION="v2.10.0"
+DEFAULT_DRIVER_VERSION="v2.11.0"
 WATCHLIST=""
 
-DRIVERVERSION="csi-unity-2.10.0"
+DRIVERVERSION="csi-unity-2.11.0"
 
 # usage will print command execution help and then exit
 function usage() {
