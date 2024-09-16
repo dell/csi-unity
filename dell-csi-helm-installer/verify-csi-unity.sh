c1964cce990813efe46ab579b54e24a187988db0
Wilson Radadia <wilson.radadia@dell.com>
2024-09-16 18:37:53 +0530

    Update k8s version

--- dell-csi-helm-installer/verify-csi-unity.sh
+++ dell-csi-helm-installer/verify-csi-unity.sh
@@ -10,7 +10,7 @@
 
 # verify-csi-unity method
 function verify-csi-unity() {
-  verify_k8s_versions "1.24" "1.30"
+  verify_k8s_versions "1.25" "1.31"
   verify_openshift_versions "4.15" "4.16"
   verify_namespace "${NS}"
   verify_required_secrets "${RELEASE}-creds"
