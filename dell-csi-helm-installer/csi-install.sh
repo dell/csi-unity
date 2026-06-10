673119b9a284c6880a79ca296a6267c1026e3c16
hoppea2 <33433874+hoppea2@users.noreply.github.com>
2026-06-10 11:13:28 -0400

    Update csi-install.sh

--- dell-csi-helm-installer/csi-install.sh
+++ dell-csi-helm-installer/csi-install.sh
@@ -17,6 +17,7 @@ NODE_VERIFY=1
 VERIFY=1
 MODE="install"
 DEFAULT_VERSION="v2.17.0"
+DRIVER="csi-unity"
 WATCHLIST=""
 
 # usage will print command execution help and then exit
@@ -143,7 +144,6 @@ else
 fi
 
 DRIVERDIR="${SCRIPTDIR}/../helm-charts/charts"
-DRIVER="csi-unity"
 VERIFYSCRIPT="${SCRIPTDIR}/verify.sh"
 
 # export the name of the debug log, so child processes will see it
